dc230be27319e3cc43f5afd2fb8955ff5d5234bb
dabcoder <dbouchare@gmail.com>
2018-11-05 22:18:11 +0100

    Add languages and cleanup

--- countries.go
+++ countries.go
@@ -2,18 +2,18 @@ package main
 
 import (
 	"encoding/json"
-	"flag"
 	"fmt"
 	"io/ioutil"
 	"log"
 	"net/http"
 	"os"
+	"strings"
 )
 
 type Country struct {
 	Capital  string         `json:"capital"`
 	Currency []CurrencyJson `json:"currencies"`
-	//Language string
+	Language []LanguageJson `json:"languages"`
 }
 
 type CurrencyJson struct {
@@ -21,21 +21,25 @@ type CurrencyJson struct {
 	CurrencySymbol string `json:"symbol"`
 }
 
-type countryResp struct {
-	Collection []Country
+type LanguageJson struct {
+	LanguageName string `json:"name"`
 }
 
 func main() {
 	const baseURL = "https://restcountries.eu/rest/v2"
-	countryName := os.Args[1]
-	flag.Parse()
-	resp, err := http.Get(baseURL + "/name/" + countryName + "?fullText=true")
+	var fullURL string
+	countryName := os.Args[1:]
+	if len(countryName) > 1 {
+		countryWeb := strings.Join(countryName, "%20")
+		fullURL = baseURL + "/name/" + countryWeb + "?fullText=true"
+	} else {
+		fullURL = baseURL + "/name/" + countryName[0] + "?fullText=true"
+	}
+	resp, err := http.Get(fullURL)
 	if err != nil {
 		fmt.Println("There was an issue completing this request")
 		log.Fatal(err)
 	}
-	fmt.Println(baseURL + "/name/" + countryName + "?fullText=true")
-	fmt.Println(resp)
 
 	body, readErr := ioutil.ReadAll(resp.Body)
 	if readErr != nil {
@@ -45,5 +49,8 @@ func main() {
 	countries := make([]Country, 0)
 	json.Unmarshal(body, &countries)
 
-	fmt.Println("Capital, currency and symbol: ", countries)
+	fmt.Println("Capital:", countries[0].Capital)
+	fmt.Println("Currency:", countries[0].Currency[0].CurrencyName)
+	fmt.Println("Symbol:", countries[0].Currency[0].CurrencySymbol)
+	fmt.Println("Language:", countries[0].Language[0].LanguageName)
 }
